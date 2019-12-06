ba8252ce9ef7ad3521d39784882e40296de26c2d
Rick Bowen <rick.bowen@rapticore.com>
2019-12-06 11:28:33 -0800

    Initial commit for easy setup script!

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+yarn global upgrade yarn@latest lerna@latest
+yarn global add lerna typescript eslint tslint jest
+yarn config set workspaces-experimental true
+yarn run setup-cli
+
+mkdir -vp .pip_cache
+pip_install() {
+  pip3 install --quiet --upgrade --compile --cache-dir .pip_cache --disable-pip-version-check --force-reinstall "$@"
+}
+
+pip_install awscli cfn-flip
