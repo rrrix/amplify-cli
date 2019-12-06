25e07b0750a87c66b42708075d55be400a5f7ff1
Rick Bowen <rick.bowen@rapticore.com>
2019-12-06 15:19:34 -0800

    Added 'cd-build' command for parallel build and deployment with CodeBuild/CodePipeline

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+set -e
 yarn global upgrade yarn@latest npm@latest
 yarn global add lerna typescript eslint tslint jest
 yarn config set workspaces-experimental true
-yarn run setup-cli
+yarn
+yarn run cd-build
+yarn run rm-cli-link
+yarn run link-cli
+yarn run link-caa
 
 mkdir -vp .pip_cache
 pip_install() {
